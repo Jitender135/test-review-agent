846583ebee1304e88378787910d1c47ba1f4c12e
Jitender Singh <147338673+Jitender135@users.noreply.github.com>
2026-06-27 22:14:05 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def divide(a, b):
+    return a / b
+
+def get_user(users, index):
+    return users[index]
+
+password = "admin123"
+api_key = "sk-abc123secretkey"
+
+def login(username, password):
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    return query
